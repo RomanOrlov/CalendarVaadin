4b118fe43971a2d78915d17c69f416f674f4528d
Roman Orlov <asindor@gmail.com>
2016-02-21 06:14:04 +0300

    С нуля + исправление в sql запросе (О Коже, мне пришлось переименовывать папку пользователя, лезть в regedit, только чтобы запустить эту срань!)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
+import CalendarVaadin.CalendarTest;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
@@ -16,24 +18,16 @@ import javax.servlet.annotation.WebServlet;
 
 public class App extends UI {
 
-
-
-
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-
+        new Navigator(this,this);
+        getUI().getNavigator().addView("CalendarTest", CalendarTest.class);
+        getUI().getNavigator().navigateTo("CalendarTest");
     }
-
-
 
     @WebServlet(urlPatterns = "/*", name = "AppServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = App.class, productionMode = false)
-    public static class AppServlet extends VaadinServlet
-    {
-
-
-
-    }
+    public static class AppServlet extends VaadinServlet {}
 
 
 
