498000f8172ecd361ab6f7fda84abda80e8a8a62
Roman Orlov <asindor@gmail.com>
2016-02-23 00:59:22 +0300

    CalendarVaadin

---
+++ target/classes/VAADIN/widgetsets/CalendarVaadin.MyAppWidgetset/deferredjs/65FCD202B8A81ABB14D42CB809C847BF/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.CalendarVaadin_MyAppWidgetset.runAsyncCallback2('ndb(1600,1,Y$d);_.vc=function Igc(){P1b((!I1b&&(I1b=new U1b),I1b),this.a.d)};yUd(Th)(2);\n//# sourceURL=CalendarVaadin.MyAppWidgetset-2.js\n')
